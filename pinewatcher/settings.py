8f5144de49c9b04d0708afc2bb4cd562bcbe38b2
radekkozera <radek.kozera1@gmail.com>
2024-03-06 19:00:18 +0100

    feat: preliminary postgresql setup

--- pinewatcher/settings.py
+++ pinewatcher/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,11 +80,14 @@ WSGI_APPLICATION = "pinewatcher.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get('DJANGO_DATABASE_NAME'),
+        "USER": os.environ.get('DJANGO_DATABASE_USER'),
+        "PASSWORD": os.environ.get('DJANGO_DATABASE_PASSWORD'),
+        "HOST": os.environ.get('DJANGO_DATABASE_HOST'),
+        "PORT": os.environ.get('DJANGO_DATABASE_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
