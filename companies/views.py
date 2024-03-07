351ffb765654d5636d3c3f253889004b8604c1a6
radekkozera <radek.kozera1@gmail.com>
2024-03-07 22:38:59 +0100

    docker config

--- companies/views.py
+++ companies/views.py
@@ -8,31 +8,25 @@ from .models import Company
 
 @method_decorator(csrf_exempt, name="dispatch")
 def create_company(request):
-    if request.method == "POST":
+    if request.method != "POST":
+        return JsonResponse({"error": "Invalid request"}, status=400)
 
-        data = json.loads(request.body.decode("utf-8"))
+    data = json.loads(request.body.decode("utf-8"))
+    name = data.get("name")
+    industry = data.get("industry")
+    location = data.get("location")
+    description = data.get("description")
+    contact_person = data.get("contact_person")
+    krs = data.get("krs")
 
-        name = data.get("name")
+    company = Company.objects.create(
+        name=name,
+        industry=industry,
+        location=location,
+        description=description,
+        contact_person=contact_person,
+        krs=krs,
+    )
 
-        industry = data.get("industry")
+    return JsonResponse({"id": company.id}, status=201)
 
-        location = data.get("location")
-
-        description = data.get("description")
-
-        contact_person = data.get("contact_person")
-
-        krs = data.get("krs")
-
-        company = Company.objects.create(
-            name=name,
-            industry=industry,
-            location=location,
-            description=description,
-            contact_person=contact_person,
-            krs=krs,
-        )
-
-        return JsonResponse({"id": company.id}, status=201)
-    
-    return JsonResponse({"error": "Invalid request"}, status=400)
